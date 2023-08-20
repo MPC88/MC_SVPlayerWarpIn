b4002b2ad140c08dc74f72bcb42fdd59dca721ed
MPC88 <myemail@gmail.com>
2023-08-20 21:54:18 +0100

    version

--- MC_SVPlayerWarpIn/Main.cs
+++ MC_SVPlayerWarpIn/Main.cs
@@ -11,7 +11,7 @@ namespace MC_SVPlayerWarpIn
     {
         public const string pluginGuid = "mc.starvalor.playerwarpin";
         public const string pluginName = "SV Player Warpin";
-        public const string pluginVersion = "1.0.0";
+        public const string pluginVersion = "1.0.1";
 
         private static bool doWarp = false;
         private static bool jumpGateWarp = false;
